0e3d9752e22d0ac41f72b76129209f08ab66ded0
NeptuneKing <samuelchngweiming@hotmail.com>
2021-06-05 10:57:18 +0800

    w

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -61,7 +61,7 @@ const Home: React.FC = () => {
     <Page>
       <Hero>
         <Heading as="h1" size="xl" mb="24px" color="secondary">
-          {TranslateString(576, 'Enter the Wolves Den')}
+          {TranslateString(576, 'Wolves Den Defi')}
         </Heading>
         <Text>{TranslateString(578, 'Next Generation Yield farm on Binance Smart Chain.')}</Text>
       </Hero>
